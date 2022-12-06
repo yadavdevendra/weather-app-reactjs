590a4d45450f7dc3c5c141df3107e75697399df3
yadavdevendra <devendrayadav2022aug@cedcommerce.com>
2022-12-06 19:25:09 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    'X-RapidAPI-Key': '30a3041379msh6f505a4cc577b4bp1a0e6djsn7f45c73a4719',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f16a2077023792cc79d0b4140967d82a"; // enter your key from openweather API
